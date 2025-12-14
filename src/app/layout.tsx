dd0d0e2d38e72ac231c0e7e8f3e1e7f8b391799d
ItsNeil17 <neil@willofsteel.me>
2025-12-15 00:21:23 +0530

    fix: hydration error with root layout
    Signed-off-by: ItsNeil17 <neil@willofsteel.me>

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -6,7 +6,5 @@ const inter = Inter({
 });
 
 export default function Layout({ children }: { children: React.ReactNode }) {
-  return (
-      <body className="flex min-h-screen flex-col">{children}</body>
-  );
+  return children;
 }
